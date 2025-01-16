e567fd86fe1cac1cf439b201f485cf677875928b
Anurag Dubey <71247280+Anuragd275@users.noreply.github.com>
2025-01-17 00:04:37 +0530

    Update env.py

--- env.py
+++ env.py
@@ -1,8 +1,8 @@
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 
 
-load_dotenv()
+# load_dotenv()
 
 # API_ID = os.getenv('API_ID')
 # API_HASH = os.getenv('API_HASH')
@@ -12,10 +12,10 @@ load_dotenv()
 # DESTINATION_CHANNEL = os.getenv('DESTINATION_CHANNEL')
 
 
-API_ID = 1
-API_HASH = "2"
+API_ID = 19039081
+API_HASH = "99a625135a721998787c2230acdd587b"
 BOT_TOKEN = "7442307482:AAF_2YEqDUTw714_8rvMX9sCZxMlspJlU4o"
-SOURCE_CHANNEL = "https://t.me/testchannel11222"
-DESTINATION_CHANNEL = ["https://t.me/tg_automation"]
+# SOURCE_CHANNEL = "https://t.me/testchannel11222"
+# DESTINATION_CHANNEL = ["https://t.me/tg_automation"]
 
-CONNECTION_STRING = os.getenv("CONNECTION_STRING")
+CONNECTION_STRING = "mongodb+srv://anuragd275:rJ10QcHOdkjW44F5@mbcluster.ytii2.mongodb.net/?retryWrites=true&w=majority&appName=MBCluster
